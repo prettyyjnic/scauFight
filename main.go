e5b304e503144f709772d06cf68b7323d7dc9b97
prettyyjnic1206 <prettyyjnic@qq.com>
2016-12-15 10:41:14 +0800

    update

--- main.go
+++ main.go
@@ -1,73 +1,86 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net/http"
+
 	"io/ioutil"
+
+	"github.com/axgle/mahonia"
 	"github.com/mozillazg/request"
-	  "golang.org/x/text/encoding/simplifiedchinese"
-    "golang.org/x/text/transform"
 )
 
 const HTTP_MAX_TRY_TIMES = 3
 
-var(
-	baseURL string
-	xuehao string
+var (
+	baseURL  string
+	xuehao   string
 	password string
+	enc      mahonia.Encoder
+	dec      mahonia.Decoder
 )
 
-func init(){
-	baseURL = "http://202.116.160.166"
+func init() {
+	enc = mahonia.NewEncoder("gbk")
+	dec = mahonia.NewDecoder("gbk")
+	// baseURL = "http://202.116.160.166"
+	baseURL = "http://202.116.160.170"
 	xuehao = "201623251128"
 	password = "1296580449xy"
 }
 
-func post(url string, data map[string]string, cookies []*http.Cookie)([]byte, []*http.Cookie, error){
+func post(url string, data map[string]string, cookies []*http.Cookie) ([]byte, []*http.Cookie, error) {
 	c := new(http.Client)
 	req := request.NewRequest(c)
-	if data!=nil {// 转化为gbk
-		
+	var tmp map[string]string
+	tmp = make(map[string]string)
+	if data != nil { // 转化为gbk
+		for k, v := range data {
+			// fmt.Println(enc.ConvertString(k))
+			// fmt.Println(enc.ConvertString(v))
+			tmp[enc.ConvertString(k)] = enc.ConvertString(v)
+		}
 	}
-	req.Data = data
+	req.Data = tmp
 	var err error
 	var resp *request.Response
-	for hasTryTimes := 1;  hasTryTimes< HTTP_MAX_TRY_TIMES; hasTryTimes++ {// 重试3次
+	for hasTryTimes := 1; hasTryTimes < HTTP_MAX_TRY_TIMES; hasTryTimes++ { // 重试3次
 		resp, err = req.Post(url)
-		if err==nil {
+		if err == nil {
 			defer resp.Body.Close()
 			break
 		}
 	}
-	if err!=nil {
-		return nil,nil,err
+	if err != nil {
+		return nil, nil, err
 	}
-	respBytes, _ := ioutil.ReadAll(transform.NewReader(resp.Body, simplifiedchinese.GBK.NewEncoder()))
+	respReader := dec.NewReader(resp.Body)
+	respBytes, _ := ioutil.ReadAll(respReader)
 	return respBytes, resp.Cookies(), nil
 }
 
-func loginIn()(map[string]string, error){
+func loginIn() (map[string]string, error) {
 	var loginUrl string
 	loginUrl = baseURL + "/default_ysdx.aspx"
 	loginData := map[string]string{
-		"TextBox1": xuehao,
-		"TextBox2":   password,
-		"RadioButtonList1":   "学生",
-		"Button1":   "登录",
+		"TextBox1":         xuehao,
+		"TextBox2":         password,
+		"RadioButtonList1": "学生",
+		"Button1":          "登录",
 	}
-	respBytes,cookies, err:= post(loginUrl, loginData, nil)
-	if err!=nil {
-		fmt.Println("error:",err.Error())
+	respBytes, cookies, err := post(loginUrl, loginData, nil)
+	if err != nil {
+		fmt.Println("error:", err.Error())
 	}
-	fmt.Println("resp:",string(respBytes))
-	fmt.Println("cookies:",cookies)
-	return nil,nil
+
+	fmt.Println("resp:", string(respBytes))
+	fmt.Println("cookies:", cookies)
+	return nil, nil
 }
 
-
-func main()  {
-	_, err :=loginIn()
-	if err!=nil {
+func main() {
+	_, err := loginIn()
+	if err != nil {
 		fmt.Println(err.Error())
 	}
 	// fmt.Println("hello world");
